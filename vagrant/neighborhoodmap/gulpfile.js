32b49f1eda63406bab4b3033a0593c619401d6c1
Gaurav Singh <gauravsinghaec@gmail.com>
2018-01-09 14:05:46 +0530

    Add files and diretories for Gulp Build tool

---
+++ vagrant/neighborhoodmap/gulpfile.js
@@ -0,0 +1,85 @@
+//below line tells eslint not to mark require as wrong thinkink it runs in browser
+/*eslint-env node */
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var eslint = require('gulp-eslint');
+var browserSync = require('browser-sync').create();
+// var jasmine = require('gulp-jasmine-phantom');
+
+//order plugin is used to order the files to be concatenated
+var order = require('gulp-order');
+//concat plugin is used to concat all JavaScript files into one 
+var concat = require('gulp-concat');
+//uglify plugin is used for minification
+var uglify = require('gulp-uglify');
+
+// gulp.task('tests', function() {
+// 	return gulp.src('spec/test.js')
+// 		.pipe(jasmine({
+// 			integration: true,
+// 			vendor: 'js/**/*.js'
+// 		}));
+// });
+
+gulp.task('default',['copy-html','scripts','scripts-dist','styles','lint'], function(){
+	// Below code wathes for any change in .scss file 
+	// and executes the styles task whenever sass files change
+	gulp.watch('sass/**/*.scss',['styles']);
+	gulp.watch('js/**/*.js',['lint']);
+	gulp.watch('/index.html',['copy-html']);
+	gulp.watch('./dist/index.html').on('change',browserSync.reload);
+	browserSync.init({server: './dist'});
+});
+
+gulp.task('dist',['copy-html','styles','lint','scripts-dist']);
+
+gulp.task('scripts',function(){
+	gulp.src('js/**/*.js')
+		.pipe(concat('all.js'))
+		.pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('scripts-dist',function(){
+	gulp.src('js/**/*.js')
+		.pipe(order([
+			'js/lib/jQuery.js',
+			'js/lib/knockout-3.2.0.js',
+			'js/app.js'
+		],{ base: './' }))	
+		.pipe(concat('all.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('copy-html',function(){
+	gulp.src('./index.html')
+		.pipe(gulp.dest('./dist'));
+});
+
+gulp.task('lint', function() {
+	// ESLint ignores files with "node_modules" paths. 
+	// So, it's best to have gulp ignore the directory as well. 
+	// Also, Be sure to return the stream from the task; 
+	// Otherwise, the task may end before the stream has finished. 
+	return gulp.src(['**/*.js','!node_modules/**','!js/lib/**'])
+		// eslint() attaches the lint output to the "eslint" property 
+		// of the file object so it can be used by other modules. 
+		.pipe(eslint())
+		// eslint.format() outputs the lint results to the console. 
+		// Alternatively use eslint.formatEach() (see Docs). 
+		.pipe(eslint.format())
+		// To have the process exit with an error code (1) on 
+		// lint error, return the stream and pipe to failAfterError last. 
+		.pipe(eslint.failAfterError());
+});
+
+gulp.task('styles',function(){
+	gulp.src('sass/**/*.scss')
+		.pipe(sass())
+		.pipe(autoprefixer( { browsers: ['last 2 versions']} ))
+		.pipe(gulp.dest('dist/css'));
+});
+
+
+browserSync.stream();
